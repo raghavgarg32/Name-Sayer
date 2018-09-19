bdd238fb7e5454342d69fb7bad2af21c4b6a2c26
Raghav <raghavgarg32@gmail.com>
2018-09-19 14:01:33 +1200

    Adding timer and confirm view

--- src/namesayer/RecordView.java
+++ src/namesayer/RecordView.java
@@ -1,16 +1,42 @@
 package namesayer;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+import javax.sound.midi.SysexMessage;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Controller for the recording scene
  */
 public class RecordView {
     @FXML
+    Label recordLabel;
+
+    @FXML
     public void handleRecordButton() throws IOException {
-        Main.changeSceneRecord();
+        recordLabel.setText("Recording now...");
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask()
+        {
+            public void run()
+            {
+                Platform.runLater(new Runnable() {
+                    public void run() {
+                        recordingEnd();
+                    }
+                });            }
+
+        };
+        timer.schedule(task,5000l);
+
     }
 
+    public void recordingEnd(){
+        recordLabel.setText("Record DONE");
+
+    }
 }
