7ddc14668eceef627da53d2f29f4c36f810c8a36
Sean Spires <sspi841@en-368398.uoa.auckland.ac.nz>
2018-09-23 18:07:52 +1200

    finished play functionality

--- src/namesayer/DataBaseController.java
+++ src/namesayer/DataBaseController.java
@@ -24,181 +24,134 @@ import java.util.regex.Pattern;
 
 public class DataBaseController implements Initializable {
 
-    public static List<String> _practiceSelection = new ArrayList<>();
+	public static List<String> _practiceSelection = new ArrayList<>();
 
+	private static ArrayList<String> nameArrayList = new ArrayList<>();
 
-    private static ArrayList<String> nameArrayList = new ArrayList<>();
-    
-    private static List<String> databaseList = new ArrayList<>();
+	private static List<String> databaseList = new ArrayList<>();
 
-    @FXML
-    private ListView<String> _creationList;
+	@FXML
+	private ListView<String> _creationList;
 
-    private static ObservableList<String> list;
+	private static ObservableList<String> list;
 
-    @FXML
-    private Button practiceBtn;
+	@FXML
+	private Button practiceBtn;
 
+	@FXML
+	public void handlePracticeBtn() {
+		if (_practiceSelection.size() == 0) {
+			Alert selectionAlert = new Alert(Alert.AlertType.NONE, "Please select" + " something", ButtonType.OK);
+			selectionAlert.showAndWait();
+			if (selectionAlert.getResult() == ButtonType.OK) {
+				selectionAlert.close();
+			}
+		} else {
+			Main.changeScenePractice();
+		}
+	}
 
-    @FXML
-    public void handlePracticeBtn() {
-        System.out.println(_practiceSelection);
-        if(_practiceSelection.size() > 1) {
-            Alert randomizeAlert = new Alert(Alert.AlertType.NONE, "Would you like" +
-                    " to randomise your selection",ButtonType.NO, ButtonType.YES);
-            randomizeAlert.showAndWait();
-            if(randomizeAlert.getResult() == ButtonType.YES) {
-                Collections.shuffle(_practiceSelection);
-                randomizeAlert.close();
-                Main.changeScenePractice();
-            }
-            else if(randomizeAlert.getResult() == ButtonType.NO) {
-                randomizeAlert.close();
-                Main.changeScenePractice();
-            }
-        }
-        else if(_practiceSelection.size() == 1) {
-        	Main.changeScenePractice();
-        }
-        else if(_practiceSelection.size() == 0) {
-            Alert selectionAlert = new Alert(Alert.AlertType.NONE, "Please select" +
-                    " something" ,ButtonType.OK);
-            selectionAlert.showAndWait();
-            if(selectionAlert.getResult() == ButtonType.OK) {
-                selectionAlert.close();
-            }
-        }
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		BashCommandWorker settingUpDatabaseWorker = new BashCommandWorker(
+				"cd Database;\n" + "\n" + "for i in $(ls); do\n" + "\n"
+						+ "names=$(echo $i | awk -F\"_\" '{print $NF}')\n" + "nameWithourExtension=\"${names%.*}\"\n"
+						+ "    echo \"This is i $i \"\n" + "    mkdir \"$nameWithourExtension\"\n" + "    \n"
+						+ "    cd \"$nameWithourExtension\"\n" + "    \n" + "    mkdir \"Database-Recordings\"\n"
+						+ "    \n" + "    mkdir \"User-Recordings\"\n" + "\n" + "    mkdir \"Ratings\"\n" + "    \n"
+						+ "    mv \"../$i\" \"./Database-Recordings\"\n" + "\n" + "    cd ..\n" + "\n" + "done\n");
 
+		_creationList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
+		_creationList.setCellFactory(CheckBoxListCell.forListView(new Callback<String, ObservableValue<Boolean>>() {
+			@Override
+			public ObservableValue<Boolean> call(String item) {
+				BooleanProperty itemState = new SimpleBooleanProperty();
+				itemState.addListener((obs, wasSelected, isNowSelected) -> {
+					if (_practiceSelection.contains(item) || isNowSelected == true) {
+						_practiceSelection.add(item);
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        BashCommandWorker settingUpDatabaseWorker = new BashCommandWorker("cd Database;\n" +
-                "\n" +
-                "for i in $(ls); do\n" +
-                "\n" +
-                "names=$(echo $i | awk -F\"_\" '{print $NF}')\n" +
-                "nameWithourExtension=\"${names%.*}\"\n" +
-                "    echo \"This is i $i \"\n" +
-                "    mkdir \"$nameWithourExtension\"\n" +
-                "    \n" +
-                "    cd \"$nameWithourExtension\"\n" +
-                "    \n" +
-                "    mkdir \"Database-Recordings\"\n" +
-                "    \n" +
-                "    mkdir \"User-Recordings\"\n" +
-                "\n" +
-                "    mkdir \"Ratings\"\n" +
-                "    \n" +
-                "    mv \"../$i\" \"./Database-Recordings\"\n" +
-                "\n" +
-                "    cd ..\n" +
-                "\n" +
-                "done\n");
+					} else if (isNowSelected == false) {
+						_practiceSelection.remove(item);
+					}
+				});
+				return itemState;
+			}
+		}));
 
-        _creationList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-       
-        
-        _creationList.setCellFactory(CheckBoxListCell.forListView(new Callback<String, ObservableValue<Boolean>>() {
-            @Override
-            public ObservableValue<Boolean> call(String item) {
-                BooleanProperty itemState = new SimpleBooleanProperty();
-                itemState.addListener((obs, wasSelected, isNowSelected) -> {
-                    if (_practiceSelection.contains(item) || isNowSelected == true ) {
-                        _practiceSelection.add(item);
-                        
-                    }
-                    else if(isNowSelected == false) {
-                        _practiceSelection.remove(item);
-                    }
-                });
-                return itemState;
-            }
-        }));
+		// This is just test data for the list
+		list = FXCollections.observableArrayList();
+		_creationList.setItems(list);
+		gettingRecordings();
+	}
 
+	public void gettingRecordings() {
+		// This swingworker gets all of the creations from the NameSayer directory
+		SwingWorker gettingRecordingsWorker = new SwingWorker<ArrayList<String>, Integer>() {
 
-        // This is just test data for the list
-        list = FXCollections.observableArrayList();
-        _creationList.setItems(list);
-        gettingRecordings();
-    }
+			@Override
+			protected ArrayList<String> doInBackground() throws Exception {
+				ArrayList<String> nameList = new ArrayList<String>();
 
-    public void gettingRecordings(){
-        //This swingworker gets all of the creations from the NameSayer directory
-        SwingWorker gettingRecordingsWorker = new SwingWorker<ArrayList<String>, Integer>() {
+				try {
+					ProcessBuilder builder = new ProcessBuilder("/bin/sh", "-c",
+							"cd Database;\n" + "\n" + "for i in $(ls); do\n" + "cd $i\n" + "\n"
+									+ "cd Database-Recordings\n" + "\n" + "ls -1 *.wav | sed -e 's/\\..*$//'\n"
+									+ "cd ..\n" + "cd ..\n" + "\n" + "done");
+					Process process = builder.start();
 
-            @Override
-            protected ArrayList<String> doInBackground() throws Exception {
-                ArrayList<String> nameList = new ArrayList<String>();
+					InputStream stdout = process.getInputStream();
+					BufferedReader stdoutBuffered = new BufferedReader(new InputStreamReader(stdout));
 
-                try {
-                    ProcessBuilder builder = new ProcessBuilder("/bin/sh", "-c", "cd Database;\n" +
-                            "\n" +
-                            "for i in $(ls); do\n" +
-                            "cd $i\n" +
-                            "\n" +
-                            "cd Database-Recordings\n" +
-                            "\n" +
-                            "ls -1 *.wav | sed -e 's/\\..*$//'\n" +
-                            "cd ..\n" +
-                            "cd ..\n" +
-                            "\n" +
-                            "done");
-                    Process process = builder.start();
+					String line = null;
 
-                    InputStream stdout = process.getInputStream();
-                    BufferedReader stdoutBuffered = new BufferedReader(new InputStreamReader(stdout));
+					while ((line = stdoutBuffered.readLine()) != null) {
+						databaseList.add(line);
+						line = line.substring(line.lastIndexOf('_') + 1);
+						nameArrayList.add(line);
 
-                    String line = null;
+						if (list.contains(line)) {
+							System.out.println(line);
+							int occurrences = Collections.frequency(nameArrayList, line);
+							System.out.println(occurrences);
+							line = line + '-' + occurrences;
+						}
+						list.add(line);
+					}
+					stdoutBuffered.close();
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				}
+				return null;
+			}
 
-                    while ((line = stdoutBuffered.readLine()) != null) {
-                    	databaseList.add(line);
-                        line = line.substring(line.lastIndexOf('_') + 1);
-                        nameArrayList.add(line);
+		};
+		gettingRecordingsWorker.execute();
+	}
 
-                        if (list.contains(line)){
-                            System.out.println(line);
-                            int occurrences = Collections.frequency(nameArrayList, line);
-                            System.out.println(occurrences);
-                            line =  line + '-' + occurrences;
-                        }
-                        list.add(line);
-                    }
-                    stdoutBuffered.close();
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                }
-                return null;
-            }
+	public static ObservableList<String> getItemList() {
+		ObservableList<String> items = FXCollections.observableArrayList();
+		for (String name : _practiceSelection) {
+			System.out.println(name);
+			items.add(name);
+		}
 
-        };
-        gettingRecordingsWorker.execute();
-    }
+		return items;
+	}
 
-    public static ObservableList<String> getItemList(){
-        ObservableList<String> items =FXCollections.observableArrayList ();
-        for (String name : _practiceSelection){
-            System.out.println(name);
-            items.add(name);
-        }
+	public static ArrayList<String> getNamesWithoutNumbers() {
+		System.out.println("Array list  " + nameArrayList);
 
-        return items;
-    }
+		return nameArrayList;
+	}
 
-    public static ArrayList<String> getNamesWithoutNumbers(){
-        System.out.println("Array list  " + nameArrayList);
+	public static ObservableList<String> getNamesWithNumbers() {
+		System.out.println("Array list  " + list);
 
-        return nameArrayList;
-    }
+		return list;
+	}
 
-    public static ObservableList<String> getNamesWithNumbers(){
-        System.out.println("Array list  " + list);
-
-        return list;
-    }
-    
-    public static List<String> getDatabaseList() {
-    	return databaseList;
-    }
+	public static List<String> getDatabaseList() {
+		return databaseList;
+	}
 }
--- src/namesayer/PracticeMenuController.java
+++ src/namesayer/PracticeMenuController.java
@@ -18,6 +18,11 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
 import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
@@ -26,6 +31,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -42,8 +48,6 @@ public class PracticeMenuController implements Initializable {
 	private static String currentName;
 
 	private ObservableList<String> userRecordingsList;
-	
-	private boolean pracListClicked = true;
 
 	@FXML
 	private Button playBtn;
@@ -59,31 +63,99 @@ public class PracticeMenuController implements Initializable {
 
 	@FXML
 	public void handlePlayButton() throws IOException {
-		if (practiceList.getSelectionModel().isEmpty()) {
+		if (practiceList.getSelectionModel().isEmpty() && userCreations.getSelectionModel().isEmpty()) {
 			Alert alert = new Alert(Alert.AlertType.NONE, "Please make a selection " + "to play", ButtonType.OK);
 			alert.showAndWait();
 			if (alert.getResult() == ButtonType.OK) {
 				alert.close();
 			}
-		} else {
-			if (!(userCreations.getSelectionModel().isEmpty())) {
-				String path = userCreations.getSelectionModel().getSelectedItem();
-				System.out.println(path);
-				Media audio = new Media(
-						new File("Database/" + currentName + "/User-Recordings/" + path).toURI().toString());
-				MediaPlayer mediaPlayer = new MediaPlayer(audio);
-				mediaPlayer.setAutoPlay(true);
-			}
-			else {
-				String path = practiceList.getSelectionModel().getSelectedItem();
-				// Path here is wrong idk how to get the prefix working i.e. the se206_... part
-				Media audio = new Media(
-						new File("Database/" + currentName + "/Database-Recordings/" + path).toURI().toString());
-				MediaPlayer mediaPlayer = new MediaPlayer(audio);
-				mediaPlayer.setAutoPlay(true);
-			}
-		}
+		} else if (practiceList.getSelectionModel().isEmpty() && !(userCreations.getSelectionModel().isEmpty())) {
+			String name = userCreations.getSelectionModel().getSelectedItem();
 
+			String pathToFile = "Database/" + PracticeMenuController.getCurrentName() + "/User-Recordings/" + name;
+
+			AudioInputStream stream;
+			AudioFormat format;
+			DataLine.Info info;
+			SourceDataLine sourceLine;
+
+			try {
+				stream = AudioSystem.getAudioInputStream(new File(pathToFile));
+				format = stream.getFormat();
+
+				info = new DataLine.Info(SourceDataLine.class, format);
+				sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+				sourceLine.open(format);
+
+				sourceLine.start();
+
+				int nBytesRead = 0;
+				int BUFFER_SIZE = 128000;
+				byte[] abData = new byte[BUFFER_SIZE];
+				while (nBytesRead != -1) {
+					try {
+						nBytesRead = stream.read(abData, 0, abData.length);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					if (nBytesRead >= 0) {
+						@SuppressWarnings("unused")
+						int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
+					}
+				}
+
+				sourceLine.drain();
+				sourceLine.close();
+
+			} catch (Exception e) {
+
+			}
+		} else if (userCreations.getSelectionModel().isEmpty() && !(practiceList.getSelectionModel().isEmpty())) {
+			String name = practiceList.getSelectionModel().getSelectedItem();
+			List<String> databaseList = DataBaseController.getDatabaseList();
+			List<String> nameList = DataBaseController.getNamesWithNumbers();
+
+			String path = databaseList.get(nameList.indexOf(name));
+			String pathToFile = "Database/" + name + "/Database-Recordings/" + path + ".wav";
+
+			AudioInputStream stream;
+			AudioFormat format;
+			DataLine.Info info;
+			SourceDataLine sourceLine;
+
+			try {
+				stream = AudioSystem.getAudioInputStream(new File(pathToFile));
+				format = stream.getFormat();
+
+				info = new DataLine.Info(SourceDataLine.class, format);
+				sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+				sourceLine.open(format);
+
+				sourceLine.start();
+
+				int nBytesRead = 0;
+				int BUFFER_SIZE = 128000;
+				byte[] abData = new byte[BUFFER_SIZE];
+				while (nBytesRead != -1) {
+					try {
+						nBytesRead = stream.read(abData, 0, abData.length);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					if (nBytesRead >= 0) {
+						@SuppressWarnings("unused")
+						int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
+					}
+				}
+
+				sourceLine.drain();
+				sourceLine.close();
+
+			} catch (Exception e) {
+
+			}
+
+		}
 	}
 
 	@FXML
@@ -102,9 +174,6 @@ public class PracticeMenuController implements Initializable {
 		} else {
 			Main.changeSceneRecord();
 		}
-		// ObservableList<String> items =FXCollections.observableArrayList (
-		// "Single", "Double", "Suite", "Family App");
-		// practiceList.setItems(items);
 	}
 
 	@FXML
@@ -144,12 +213,7 @@ public class PracticeMenuController implements Initializable {
 		userRecordings = new ArrayList<String>();
 		userRecordingsList = FXCollections.observableArrayList();
 		currentName = "Name";
-		practiceList.getItems().add("hello");
-		
-//		practiceList.setCellFactory(param -> new ListView<String> {
-//			
-//		});
-		
+
 		practiceList.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
 			@Override
@@ -200,17 +264,25 @@ public class PracticeMenuController implements Initializable {
 			}
 		});
 	}
-	
+
 	@FXML
 	public void practiceListClicked() {
-			pracListClicked = true;
-			System.out.println(pracListClicked);
+		userCreations.getSelectionModel().clearSelection();
 	}
-	
+
 	@FXML
 	public void userCreationsListClicked() {
-			pracListClicked = false;
-			System.out.println(pracListClicked);
+		practiceList.getSelectionModel().clearSelection();
+	}
+
+	@FXML
+	public void handleShuffleButton() {
+		ObservableList<String> tempList = FXCollections.observableArrayList();
+		List<String> praticeSelection = DataBaseController._practiceSelection;
+		Collections.shuffle(praticeSelection);
+		tempList.addAll(praticeSelection);
+		practiceList.setItems(tempList);
+
 	}
 
 	public static String getCurrentName() {
